a868389a62cac3aee3a6232cb1363c0e3c8ec2e5
Sachin Gyandor <gyandors@gmail.com>
2024-08-11 15:24:40 +0530

    Splitting the code in to files

--- app.js
+++ app.js
@@ -1,50 +1,9 @@
 const http = require("http");
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  // console.log(req.url, req.method, req.headers);
+const route = require("./route");
+console.log(route);
+console.log(route.text);
 
-  const url = req.url,
-    method = req.method;
-
-  if (url === "/") {
-    res.setHeader("Content-Type", "text/html");
-
-    res.write("<html>");
-    res.write("<head> <title>Form</title> </head>");
-    res.write(
-      "<body> <form action='/message' method='POST'> <input type='text' name='fname'> <input type='text' name='lname'> <button type='submit'>Send</button> </form> </body>"
-    );
-    res.write("</html>");
-
-    return res.end();
-  }
-
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-      console.log(body);
-    });
-
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      let data = parsedBody.split("=");
-      console.log(data);
-      fs.writeFileSync("text.txt", parsedBody);
-    });
-
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
-  }
-
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head> <title>Hello Node.js</title> </head>");
-  res.write("<body><h1>Hello World</h1></body>");
-  res.write("</html>");
-  res.end();
-});
+const server = http.createServer(route.requestHandler);
 
 server.listen(3000);
