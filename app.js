48586709a8fcd4df198019f65558e9768b691b17
Sachin Gyandor <gyandors@gmail.com>
2024-08-11 15:10:04 +0530

    Sending response to the client

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head> <title>Hello Node.js</title> </head>");
+  res.write("<body><h1>Hello World</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
