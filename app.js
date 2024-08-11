7364d3a752693c73d1539096836074794511632b
Sachin Gyandor <gyandors@gmail.com>
2024-08-11 15:16:06 +0530

    Working with form submission

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  // console.log(req.url, req.method, req.headers);
+
+  const url = req.url,
+    method = req.method;
+
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+
+    res.write("<html>");
+    res.write("<head> <title>Form</title> </head>");
+    res.write(
+      "<body> <form action='/message' method='POST'> <input type='text' name='fname'> <input type='text' name='lname'> <button type='submit'>Send</button> </form> </body>"
+    );
+    res.write("</html>");
+
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("text.txt", "Dummy Text");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
 
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
