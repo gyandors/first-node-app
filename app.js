645f4bf1a6f0b52ff8597e07caa62b4fc3a39dcc
Sachin Gyandor <gyandors@gmail.com>
2024-08-20 16:04:21 +0530

    Sending response back for different urls

--- app.js
+++ app.js
@@ -1,7 +1,57 @@
 const http = require("http");
 
-const route = require("./route");
+const server = http.createServer((req, res) => {
+  const url = req.url;
 
-const server = http.createServer(route.requestHandler);
+  res.setHeader("Content-type", "text/html");
 
-server.listen(3000);
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head>");
+    res.write("<title> Welcome </title>");
+    res.write("</head>");
+
+    res.write("<body>");
+    res.write("<h1> Welcome Sachin </h1>");
+    res.write("</body>");
+
+    res.write("</html>");
+  } else if (url === "/home") {
+    res.write("<html>");
+    res.write("<head>");
+    res.write("<title> Home </title>");
+    res.write("</head>");
+
+    res.write("<body>");
+    res.write("<h1> Welcome to Home page </h1>");
+    res.write("</body>");
+
+    res.write("</html>");
+  } else if (url === "/about") {
+    res.write("<html>");
+    res.write("<head>");
+    res.write("<title> About </title>");
+    res.write("</head>");
+
+    res.write("<body>");
+    res.write("<h1> Welcome to About page </h1>");
+    res.write("</body>");
+
+    res.write("</html>");
+  } else if (url === "/node") {
+    res.write("<html>");
+    res.write("<head>");
+    res.write("<title> Node </title>");
+    res.write("</head>");
+
+    res.write("<body>");
+    res.write("<h1> Welcome to Node page </h1>");
+    res.write("</body>");
+
+    res.write("</html>");
+  }
+
+  res.end();
+});
+
+server.listen(4000);
