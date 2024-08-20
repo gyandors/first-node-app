9fe0da78454017a7af0cd71914b6c99b473b4a55
Sachin Gyandor <gyandors@gmail.com>
2024-08-20 17:45:43 +0530

    Working with file system and Buffers

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
 
-  res.setHeader("Content-type", "text/html");
+  res.setHeader("content-type", "text/html");
 
   if (url === "/") {
     res.write("<html>");
     res.write("<head>");
-    res.write("<title> Welcome </title>");
+    res.write("<title> Chat </title>");
     res.write("</head>");
 
     res.write("<body>");
-    res.write("<h1> Welcome Sachin </h1>");
+    res.write(`<p> ${fs.readFileSync("chat.txt").toString()} </p>`);
+    res.write(
+      "<form action='/message' method='POST'> <input type='text' name='message' /> <button>Send</button> </form>"
+    );
     res.write("</body>");
 
     res.write("</html>");
-  } else if (url === "/home") {
+  }
+
+  if (url === "/message" && method === "POST") {
+    const data = [];
+
+    req.on("data", (chunk) => {
+      data.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedData = Buffer.concat(data).toString();
+      const message = parsedData.split("=")[1];
+      fs.writeFileSync("chat.txt", message);
+    });
+
+    res.writeHead(302, { location: "/" });
+  }
+
+  if (url === "/home") {
     res.write("<html>");
     res.write("<head>");
     res.write("<title> Home </title>");
@@ -27,7 +50,9 @@ const server = http.createServer((req, res) => {
     res.write("</body>");
 
     res.write("</html>");
-  } else if (url === "/about") {
+  }
+
+  if (url === "/about") {
     res.write("<html>");
     res.write("<head>");
     res.write("<title> About </title>");
@@ -38,7 +63,9 @@ const server = http.createServer((req, res) => {
     res.write("</body>");
 
     res.write("</html>");
-  } else if (url === "/node") {
+  }
+
+  if (url === "/node") {
     res.write("<html>");
     res.write("<head>");
     res.write("<title> Node </title>");
